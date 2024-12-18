1ad8627fba72d033295c70297506b5f7e047054f
Anya Korsakova <anya@anya-mbp-1844.calicolabs.local>
2024-12-18 14:21:18 -0800

    added config

--- find_global_max.py
+++ find_global_max.py
@@ -1,8 +1,17 @@
 import os
 from dataset import *
+import json
+import numpy as np
 
+
+config_file = "config.json"
+with open(config_file) as config_open:
+    configs = json.load(config_open)
 
 # Find global maximum across all files
-activation_pattern = "v2_activations/conv1d_1/activations_*.h5"
+activation_pattern = configs["activations_path"]
 global_max = find_global_max(activation_pattern)
 print(f"Global maximum value: {global_max}")
+
+# save to npy file
+np.save(configs["global_max_save_path"], global_max)
