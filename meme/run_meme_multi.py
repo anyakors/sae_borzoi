97924639a64cf2acd64c99201e9dfcfa44ffdb0f
Anya Korsakova <anya@anya-mbp-1844.calicolabs.local>
2025-04-01 10:11:49 -0700

    meme analysis added

--- meme/run_meme_multi.py
+++ meme/run_meme_multi.py
@@ -33,7 +33,7 @@ for im,model_folder in enumerate(model_folders):
     slurm_string += f"#SBATCH -o temp/job_{im}.out \n"
     slurm_string += f"#SBATCH -e temp/job_{im}.err \n"
     slurm_string += '#SBATCH --mem 22000 \n#SBATCH --time 2-0:0:0 \n'
-    slurm_string += f'source /home/anya/.bashrc; echo $HOSTNAME; meme-analysis.sh /home/anya/code/sae_borzoi/models/{model_folder}'
+    slurm_string += f'source /home/anya/.bashrc; echo $HOSTNAME; bash /home/anya/code/sae_borzoi/meme/meme-analysis.sh /home/anya/code/sae_borzoi/models/{model_folder}'
 
     with open(f'temp/job_{im}.sb', 'w') as f:
         f.write(slurm_string)
